5cba69d58fd60e4e396696b2e23b3e6aaba5afa2
Dmitrii <SurikovDA@yandex.ru>
2023-04-29 14:16:18 +0300

    Добавалены: -Реализация user и post через внедрение зависимостей -Логика описана в service

---
+++ src/main/java/ru/yandex/practicum/catsgram/service/PostService.java
@@ -0,0 +1,35 @@
+package ru.yandex.practicum.catsgram.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.catsgram.exceptions.UserNotFoundException;
+import ru.yandex.practicum.catsgram.model.Post;
+import ru.yandex.practicum.catsgram.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PostService {
+    UserService userService;
+    private final List<Post> posts = new ArrayList<>();
+    @Autowired
+    public PostService(UserService userService){
+        this.userService = userService;
+    }
+
+    public List<Post> findAll() {
+        return posts;
+    }
+
+    public Post create(Post post) {
+        User postAuthor = userService.findUserByEmail(post.getAuthor());
+        if (postAuthor == null) {
+            throw new UserNotFoundException(String.format(
+                    "Пользователь %s не найден",
+                    post.getAuthor()));
+        }
+        posts.add(post);
+        return post;
+    }
+}
